42be12fc7b93c4e2f1b89f3e06a54b6ca63c8247
vllyxw <victoriapansini661@gmail.com>
2025-02-21 11:45:53 -0300

    main

---
+++ src/main/java/com/faturaflow/FaturaflowApplication.java
@@ -0,0 +1,13 @@
+package com.faturaflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturaflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturaflowApplication.class, args);
+	}
+
+}
